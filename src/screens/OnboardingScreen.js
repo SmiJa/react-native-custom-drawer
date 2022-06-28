41dd0976c670fcd6e54320d96b73af86e4b4dddd
Jason Smith <jasons@alphaworks.tech>
2022-06-28 12:27:36 -0700

    chore(OnboardingScreen): change name from LetsBegin and move into screens folder

--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function LetsBegin() {
+export default function OnboardingScreen() {
   return (
     <View>
       <Text>LetsBegin</Text>
