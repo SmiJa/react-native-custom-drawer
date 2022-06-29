54dff84f1ada7b1526c8f24de85457da78169fc1
Jason Smith <jasons@alphaworks.tech>
2022-06-29 13:00:09 -0700

    feat(homescreen): change text to image in touchable opacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,17 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ImageBackground } from 'react-native'
 
 export default function HomeScreen({navigation}) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{flex: 1, justifyContent: 'space-between', paddingHorizontal: 20, alignItems: 'flex-start', flexDirection: 'row'}}>
       <Text>Home Screen</Text>
       <TouchableOpacity
         onPress={() => navigation.openDrawer()}
-        style={{
-          backgroundColor: "#34A",
-          borderRadius: 10,
-        }}
       >
-        <Text style={{color: "#fff", padding: 10}}>Open Drawer</Text>
+        <ImageBackground
+          source={require('./../assets/images/user-profile.jpg')}
+          style={{width: 35, height: 35}}
+          imageStyle={{borderRadius: 25}}
+        />
       </TouchableOpacity>
       
     </View>
